aaab00666ab5e95c48d29afa8448e19b91afaf6e
Simon <abeluga@posteo.net>
2022-12-09 16:27:33 +0100

    fix i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,10 @@
 import { resolve, dirname } from "node:path";
 import { fileURLToPath } from "url";
 import VueI18nVitePlugin from "@intlify/unplugin-vue-i18n/vite";
+import en from "./lang/en.json";
 
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/prismic"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/prismic", "@nuxtjs/i18n"],
   prismic: {
     endpoint: "https://bankgreen.prismic.io/api/v2",
   },
@@ -22,5 +23,16 @@ export default defineNuxtConfig({
   css: ["@/styles/style.css"],
   routeRules: {
     "/sustainable-ethical-banks": { redirect: "/sustainable-eco-banks" },
+  },
+  i18n: {
+    vueI18n: {
+      legacy: false,
+      globalInjection: true,
+      locale: "en",
+      messages: {
+        en,
+      },
+      fallbackLocale: "en",
+    },
   },
 });
